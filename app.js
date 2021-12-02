6336bba2743f0b99798e4fb00f7ccd6b9503ded2
armaBurton <armanarma@armaburton.com>
2021-12-02 14:33:00 -0800

    bugs fixed

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 const headDropdown = document.getElementById('head-dropdown');
 const middleDropdown = document.getElementById('middle-dropdown');
 const bottomDropdown = document.getElementById('bottom-dropdown');
-const headEl = document.getElementById('top');
+const headEl = document.getElementById('head');
+const middleEl = document.getElementById('middle');
 const bottomEl = document.getElementById('bottom');
 const reportEl = document.getElementById('report');
-const chatchphrasesEl = document.getElementById('chatch-phrases');
+const chatchphrasesEl = document.getElementById('chatchphrases');
 const catchphraseInput = document.getElementById('catchphrase-input');
+const catchphraseButton = document.getElementById(`catchphrase-button`);
 
-const headCount = 0;
-const middleCount = 0;
-const bottomCount = 0;
+let headCount = 0;
+let middleCount = 0;
+let bottomCount = 0;
 
-const catchphrases = 0;
+const catchphrases = [];
 
 headDropdown.addEventListener('change', () => {
     const value = headDropdown.value;
-
     headCount++;
-    headEl.backgroundImage = `url("./assets/${value}-head.png")`;
-    displayStats();
+    headEl.style.backgroundImage = `url("./assets/${value}-head.png")`;
+    reportEl.textContent = displayStats();
 });
 
 
 middleDropdown.addEventListener('change', () => {
     const value = middleDropdown.value;
-
     middleCount++;
-    middleEl.backgroundImage = `url("./assets/${value}-middle.png")`;
-    displayStats();
+    middleEl.style.backgroundImage = `url("./assets/${value}-middle.png")`;
+    reportEl.textContent = displayStats();
 });
 
 
@@ -35,29 +35,28 @@ bottomDropdown.addEventListener('change', () => {
     const value = bottomDropdown.value;
 
     bottomCount++;
-    bottomEl.backgroundImage = `url("./assets/${value}-pants.png")`;
-    displayStats();
+    bottomEl.style.backgroundImage = `url("./assets/${value}-pants.png")`;
+    reportEl.textContent = displayStats();
 });
 
-catchphraseButton.addEventListener = () => {
+catchphraseButton.addEventListener ('click', () => {
     const newCatchphrase = catchphraseInput.value;
     catchphrases.push(newCatchphrase);
 
     catchphraseInput.value = '';
     displayCatchphrases();
-};
+});
 
 function displayStats() {
     return `You have changed the head ${headCount} times, the body ${middleCount} times, and the pants ${bottomCount} times. And nobody can forget your character's classic catchphrases:`;
 }
 
 function displayCatchphrases() {
+    chatchphrasesEl.textContent = ``;
     for (let catchphrase of catchphrases) {
         const p = document.createElement('p');
 
-        p.add('catchphrase');
-        p.textContent = catchphrases;
-
+        p.textContent = catchphrase;
         chatchphrasesEl.append(p);
     }
 }
